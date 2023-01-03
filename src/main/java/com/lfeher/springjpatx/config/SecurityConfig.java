4b5944588032cd774c3521f62d1271ec49135d29
lfeher <laszlo.feher.mail@gmail.com>
2023-01-03 09:49:57 +0100

    SecurityConfig update

--- src/main/java/com/lfeher/springjpatx/config/SecurityConfig.java
+++ src/main/java/com/lfeher/springjpatx/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
+                        .anyRequest().hasRole("ADMIN")
                 )
                 .httpBasic(withDefaults())
                 .cors().and().csrf().disable();
